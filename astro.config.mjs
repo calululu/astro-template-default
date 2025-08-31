44bfc373fecfc6e18b267a57a52df9023c29f219
Alessandro <145708702+calululu@users.noreply.github.com>
2025-08-31 11:09:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://websiteURLexample.it",
