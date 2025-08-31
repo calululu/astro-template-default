5288ef3cc77e223f19a4ca76623b97551d3600a7
Alessandro <145708702+calululu@users.noreply.github.com>
2025-08-31 11:04:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import sanity from "@sanity/astro";
-
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    // 👇 Update these lines
-    sanity({
-      projectId: "MY-PROJECT-ID",
-      dataset: "MY-DATASET-NAME",
-      useCdn: false, // for static builds
-    }),
-  ],
+  site: "https://websiteURLexample.it",
+  vite: { plugins: [tailwindcss()] },
 });
